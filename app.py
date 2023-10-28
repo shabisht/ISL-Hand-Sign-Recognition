065ef0dd4644a7ee8fdd60b6f810b4d0b8b124f4
peskyji <peskyji.com>
2023-10-28 18:34:11 +0530

    app updated

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ import mediapipe as mp
 import tensorflow.keras as keras
 from PIL import Image
 import av
+import logging
+import os
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest import Client
+
 
 st.set_page_config(page_title='ISL Detection', page_icon=':clapper:')
 
@@ -71,6 +76,36 @@ hands = mp.solutions.hands.Hands(
 # webrtc_streamer(key="example", video_processor_factory=VideoTransformer)
 # with st.sidebar:
 #     st.info("Test")
+logger = logging.getLogger(__name__)
+def get_ice_servers():
+    """Use Twilio's TURN server because Streamlit Community Cloud has changed
+    its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
+    We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
+    but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
+    See https://github.com/whitphx/streamlit-webrtc/issues/1213
+    """
+
+    # Ref: https://www.twilio.com/docs/stun-turn/api
+    try:
+        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    except KeyError:
+        logger.warning(
+            "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    client = Client(account_sid, auth_token)
+
+    try:
+        token = client.tokens.create()
+    except TwilioRestException as e:
+        st.warning(
+            f"Error occurred while accessing Twilio API. Fallback to a free STUN server from Google. ({e})"  # noqa: E501
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    return token.ice_servers
 
 
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
@@ -112,6 +147,10 @@ model = load_model()
 webrtc_ctx = webrtc_streamer(
     key="object-detection",
     mode=WebRtcMode.SENDRECV,
+    rtc_configuration={
+        "iceServers": get_ice_servers(),
+        "iceTransportPolicy": "relay",
+    },
     video_frame_callback=video_frame_callback,
     media_stream_constraints={"video": True, "audio": False},
     async_processing=True,
