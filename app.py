8116ecd08e1d179c903b13c5810f28e777de8b91
peskyji <peskyji.com>
2023-10-28 20:30:20 +0530

    app updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, WebRtcMode
 import mediapipe as mp
-# import tensorflow.keras as keras
+import tensorflow.keras as keras
 from PIL import Image
 import av
 import logging
@@ -14,10 +14,10 @@ from twilio.rest import Client
 
 st.set_page_config(page_title='ISL Detection', page_icon=':clapper:')
 
-# @st.cache_resource
-# def load_model():
-#     model = keras.models.load_model('models/ISL2.keras')
-#     return model
+@st.cache_resource
+def load_model():
+    model = keras.models.load_model('models/ISL2.keras')
+    return model
 
 class_labels = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',
                 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11:
@@ -70,7 +70,7 @@ hands = mp.solutions.hands.Hands(
 #         img = cv2.flip(img, 1)
 #         return img
     
-# model = load_model()
+model = load_model()
 
 st.header("Indian Sign Language Symbol Detection")
 with st.sidebar:
@@ -84,28 +84,30 @@ def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     results = hands.process(img)
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            mp.solutions.draw_utis.draw_landmarks(
-                img, hand_landmarks, connections=mp.solutions.hands.HAND_CONNECTIONS
-            )
+            # mp.solutions.draw_utis.draw_landmarks(
+            #     img, hand_landmarks, connections=mp.solutions.hands.HAND_CONNECTIONS
+            # )
             x_min, y_min, x_max, y_max = np.inf, np.inf, 0, 0
             for landmark in hand_landmarks.landmark:
                 x_min, y_min = int(min(landmark.x*width, x_min)), int(min(landmark.y*height, y_min))
                 x_max, y_max = int(max(landmark.x*width, x_max)), int(max(landmark.y*height, y_max))
     
     #     # cv2.imwrite("test.jpg", img[y_min-offset:y_max+offset, x_min-offset:x_max+offset])
-    #     try:
-    #         test_img = Image.fromarray(img[y_min-offset:y_max+offset, x_min-offset:x_max+offset])
-    #         test_img = test_img.resize((250,250))
-    #         test_img = np.expand_dims(np.array(test_img), axis=0)
-    #         prediction = model.predict(test_img)
-    #         value = max(prediction[0])
-    #         label = np.argmax(prediction[0])
+        try:
+            # test_img = Image.fromarray(img[y_min-offset:y_max+offset, x_min-offset:x_max+offset])
+            # test_img = test_img.resize((250,250))
+            # test_img = np.expand_dims(np.array(test_img), axis=0)
+            # prediction = model.predict(test_img)
+            # value = max(prediction[0])
+            # label = np.argmax(prediction[0])
 
-    #         cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (255,0,0),2)
-    #         cv2.putText(img, f"{label}- {value.format('.2f')}", (x_min, y_min-offset-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
+            cv2.rectangle(img, (0, 100), (0, 100), (255,0,0),2)
+
+            # cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (255,0,0),2)
+            # cv2.putText(img, f"{label}- {value.format('.2f')}", (x_min, y_min-offset-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
         
-    #     except ValueError:
-    #         pass
+        except ValueError:
+            pass
 
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
