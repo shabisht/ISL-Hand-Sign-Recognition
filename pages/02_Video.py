a9040e0815abc624761f9ebf04843cd33cc07cbf
peskyji <peskyji.com>
2023-10-30 13:26:50 +0530

    Video.py updated

--- pages/02_Video.py
+++ pages/02_Video.py
@@ -19,6 +19,8 @@ import cv2
 import numpy as np
 import av
 import mediapipe as mp
+from PIL import Image
+import streamlit as st
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 
 mp_drawing = mp.solutions.drawing_utils
@@ -37,16 +39,38 @@ def process(image):
 
     # Draw the hand annotations on the image.
     image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    img = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    height, width, _ = img.shape
+    offset = 20
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         mp_drawing.draw_landmarks(
-            image,
+            img,
             hand_landmarks,
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
-    return cv2.flip(image, 1)
+        for landmark in hand_landmarks.landmark:
+                x_min, y_min = int(min(landmark.x*width, x_min)), int(min(landmark.y*height, y_min))
+                x_max, y_max = int(max(landmark.x*width, x_max)), int(max(landmark.y*height, y_max))
+    
+        # cv2.imwrite("test.jpg", img[y_min-offset:y_max+offset, x_min-offset:x_max+offset])
+        try:
+            test_img = Image.fromarray(img[y_min-offset:y_max+offset, x_min-offset:x_max+offset])
+            test_img = test_img.resize((250,250))
+            test_img = np.expand_dims(np.array(test_img), axis=0)
+            prediction = st.session_state.model.predict(test_img)
+            value = max(prediction[0])
+            label = np.argmax(prediction[0])
+
+            cv2.rectangle(img, (0, 100), (0, 100), (255,0,0),2)
+
+            cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (255,0,0),2)
+            cv2.putText(img, f"{st.session_state.class_labels[label]}- {value.format('.2f')}", (x_min, y_min-offset-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
+        
+        except ValueError:
+            pass
+    return img
 
 
 RTC_CONFIGURATION = RTCConfiguration(
